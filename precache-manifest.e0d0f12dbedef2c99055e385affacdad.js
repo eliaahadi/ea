c40241056c864d4d3093f8cc854a1f6d29740843
eliaahadi <elia.ahadi@gmail.com>
2020-02-23 20:08:38 -0500

    Updates

---
+++ precache-manifest.e0d0f12dbedef2c99055e385affacdad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2aae659e38d4984fd1bbe15f42f2492",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa661615c808e28c1c52",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c077240e5b6122d549e",
+    "url": "/portfolio/static/js/2.c73f47d2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.c73f47d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa661615c808e28c1c52",
+    "url": "/portfolio/static/js/main.2268a015.chunk.js"
+  },
+  {
+    "revision": "5566635d19e84539539c",
+    "url": "/portfolio/static/js/runtime-main.a04fcd5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
