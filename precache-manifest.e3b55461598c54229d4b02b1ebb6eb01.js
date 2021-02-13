beb54dad4a460f0cba4fd4df3ca8bb3e48fc35b8
eliaahadi <elia.ahadi@gmail.com>
2021-02-13 08:16:23 -0500

    Updates

--- precache-manifest.e3b55461598c54229d4b02b1ebb6eb01.js
+++ precache-manifest.e3b55461598c54229d4b02b1ebb6eb01.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0264f746c3c2e15229ca0eee5b7a1efd",
+    "revision": "cc48789a29e684f12521df662f7f4fe4",
     "url": "/ea/index.html"
   },
   {
-    "revision": "c9dd3d601c9a3eec437c",
+    "revision": "28edefe6952109c1a62c",
     "url": "/ea/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "3951c0c8a411d1aa919e",
-    "url": "/ea/static/js/2.8b08f90a.chunk.js"
+    "revision": "7d4dd4a4e549a9545c7f",
+    "url": "/ea/static/js/2.5e656911.chunk.js"
   },
   {
     "revision": "3adc01bea76e7956dc3633ee898f6936",
-    "url": "/ea/static/js/2.8b08f90a.chunk.js.LICENSE.txt"
+    "url": "/ea/static/js/2.5e656911.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c9dd3d601c9a3eec437c",
-    "url": "/ea/static/js/main.456ce698.chunk.js"
+    "revision": "28edefe6952109c1a62c",
+    "url": "/ea/static/js/main.2aebcd87.chunk.js"
   },
   {
     "revision": "d1f273a6e141b2219499",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ea/precache-manifest.84d7b364115706e8533e8f5cf72f6d5e.js"
+  "/ea/precache-manifest.e3b55461598c54229d4b02b1ebb6eb01.js"
 );
 
 self.addEventListener('message', (event) => {
